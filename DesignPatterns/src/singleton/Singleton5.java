e77887db38f58a873b6082a8e5090ae678b1fad6
sleepingxiaoming <2784416301@qq.com>
2022-10-11 14:47:08 +0800

    单例模式

---
+++ DesignPatterns/src/singleton/Singleton5.java
@@ -0,0 +1,22 @@
+package singleton;
+
+/**
+ * @description: 单例设计模式 枚举
+ * @author: xiaoming
+ * @date: 2022/10/11 13:30
+ */
+public enum Singleton5 {
+    SingleObject("test");
+
+    private String temp;
+
+
+    Singleton5(String temp) {
+        this.temp = temp;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+}
+
