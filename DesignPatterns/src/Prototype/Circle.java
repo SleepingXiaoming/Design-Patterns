188eda609efe01b3d9906879d0d78b66b751672b
sleepingxiaoming <2784416301@qq.com>
2022-10-13 21:23:45 +0800

    更新原型设计模式

---
+++ DesignPatterns/src/Prototype/Circle.java
@@ -0,0 +1,17 @@
+package Prototype;
+
+/**
+ * @description: Shape 类的实现类
+ * @author: xiaoming
+ * @date: 2022/10/13 20:40
+ */
+public class Circle extends Shape {
+    public Circle() {
+        type = "Circle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Circle Draw()");
+    }
+}
---
+++ DesignPatterns/src/Prototype/Square.java
@@ -0,0 +1,17 @@
+package Prototype;
+
+/**
+ * @description: Shape 类的实现类
+ * @author: xiaoming
+ * @date: 2022/10/13 20:38
+ */
+public class Square extends Shape {
+    public Square() {
+        type = "Square";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Square Draw()");
+    }
+}
